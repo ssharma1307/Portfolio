93eaa3d950569489ddf47df369a5ad591149b410
ssharma1307 <sharma.sukhdeep30@gmail.com>
2024-12-12 18:02:01 -0500

    Added Navbar library from bootstrap

--- personal-portfolio/src/component/NavBar.js
+++ personal-portfolio/src/component/NavBar.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
 export default function NavBar() {
+
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+     const onScroll = ()=>{
+        if(window.scrollY > 50){
+            setScrolled(true);
+        }else{
+            setScrolled(false);
+        }
+     }
+
+     window.addEventListener("scroll", onScroll);
+
+     return()=> window.removeEventListener("scroll", onScroll);
+    }, [])
+    
+
   return (
     
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
             <img src="../Images/logo.png" alt='Logo'/>
@@ -27,7 +46,9 @@ export default function NavBar() {
                 <a href="http://"><img src='' alt='' /></a>
                 <a href="http://"><img src='' alt='' /></a>
             </div>
-
+            <button className="vvd" onClick={()=> console.log('connect')}>
+                <span>Let's Connect</span>
+            </button>
           </span>
         </Navbar.Collapse>
       </Container>
